76f940073a2cdb6ceab882ab030dbcc842838049
Slavik <slavik@android-3c93a30b7563426c.(none)>
2014-02-14 02:04:24 -0500

    Finished 1.3.3  Procedures as General Methods

--- 1.35 fixed-point.scm
+++ 1.35 fixed-point.scm
@@ -63,7 +63,7 @@
 (define (phi)
   (fixed-point (lambda (phi) (+ 1 (/ 1 phi))) 1.0))
 
-(phi)
+; (phi)
 
 ; 1.36
 (define (func)
@@ -77,22 +77,75 @@
 
 ; 1.37
 
-(define (cont-frac n d k)
-  (define (iter i)
+(define (cont-frac-recursive n d k i)
     (if (> i k) 0
-        (/ (n i) (+ (d i) (iter (+ i 1)) ) ) )
+        (/ (n i) (+ (d i) (cont-frac-recursive n d k (+ i 1)) ) ) )
+)
+;(trace cont-frac-recursive)
+
+(define (cont-frac-iterative n d k)
+  (define (iter i result)
+    (if (< i 1) result
+        (iter (- i 1) (/ (n i) (+ (d i) result)) ) )
     )
   (trace iter)
-  (/ 1 (iter 1))
+  (iter k 0)
+  )
+  
+; finding 'phi' (golden ratio) using finite continued fraction:
+(define (phi2)
+  (/ 1
+  (cont-frac-recursive (lambda (i) 1.0) (lambda (i) 1.0) 12 1)
+  )
 )
+;(phi2)
 
-(define (cont-frac-recursive n d k)
-  (define (iter i)
+(define (phi3)
+  (/ 1
+  (cont-frac-iterative (lambda (i) 1.0) (lambda (i) 1.0) 12)
+  )
+)
+;(phi3)
+
+; 1.38 finding e - Euler constant
+
+(define (euler_d i)
+  (cond
+    ((= i 2) 2)
+    ((= (remainder (- i 2.0) 3) 0) (+ (euler_d (- i 3)) (euler_d (- i 2)) (euler_d (- i 1))))
+    (else 1)
+        ) )
+(trace euler_d)
+;(euler_d 1) ; 1
+;(euler_d 2) ; 2
+;(euler_d 5) ; 4
+;(euler_d 7) ; 1
+;(euler_d 11) ; 8
+;(euler_d 13) ; 1
+;(euler_d 26) ; 18
+
+(define (euler)
+  (+ 2 (cont-frac-recursive (lambda (i) 1.0) euler_d 20 1)))
+
+;(euler)
+
+; 1.39 Calculating tangent by J.H. Lambert
+
+(define (cont-frac-recursive-oper n d k i oper)
     (if (> i k) 0
-        (/ (n i) (+ (d i) (iter (+ i 1)) ) ) )
-    )
-  (/ 1 (iter 1))
+        (/ (n i) (oper (d i) (cont-frac-recursive-oper n d k (+ i 1) oper) ) ) )
 )
 
-; finding 1/phi using continued fraction:
-(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12)
+(define (tan-cf x k)
+  (define (tan-n i) (if (= i 1) x (* x x)) )
+  (define (tan-d i) (if (= i 1) 1.0 (+ 2 (tan-d (- i 1))) ) )
+  (cont-frac-recursive-oper tan-n tan-d k 1 -)
+  )
+
+(tan-cf -1 20)
+(tan-cf 0 20)
+(tan-cf 1 20)
+
+(tan-cf 2 10)
+(tan-cf 3 10)
+(tan-cf 4 10)
